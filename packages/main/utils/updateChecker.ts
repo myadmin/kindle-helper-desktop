75ca138ff94dcb75fc181e513584788883fea9b5
zhouwei <zhou21192345@163.com>
2022-06-13 22:25:45 +0800

    feat: update checker

--- packages/main/utils/updateChecker.ts
+++ packages/main/utils/updateChecker.ts
@@ -1,19 +1,28 @@
 import { dialog, shell } from 'electron';
 import axios from 'axios';
+import { lt } from 'semver'
 import pkg from '../../../package.json';
 const version = pkg.version;
-const release =
+const releaseUrl =
     'https://api.github.com/repos/myadmin/kindle-helper-desktop/releases/latest';
+const releaseUrlBackup =
+    'https://github.com/myadmin/kindle-helper-desktop/blob/develop/package.json';
 const downloadUrl =
     'https://github.com/myadmin/kindle-helper-desktop/releases/latest';
 
 const checkVersion = async () => {
-    const res = await axios.get(release);
+    let res: any;
+    try {
+        res = await axios.get(releaseUrl).catch(async () => {
+            const result = await axios.get(releaseUrlBackup);
+            return result;
+        });
+    } catch (err) {
+        console.log(err);
+    }
     if (res.status === 200) {
-        const latest = res.data.name; // 获取版本号
-        console.log('latest', latest);
+        const latest = res.data.version || res.data.name;
         const result = compareVersion2Update(version, latest); // 比对版本号，如果本地版本低于远端则更新
-        console.log('result', result);
         if (result) {
             dialog
                 .showMessageBox({
@@ -38,18 +47,18 @@ const checkVersion = async () => {
 };
 
 const compareVersion2Update = (current: string, latest: string): boolean => {
-    const currentVersion = current.split('.').map((item) => parseInt(item));
-    const latestVersion = latest.split('.').map((item) => parseInt(item));
-    let flag = false;
-    console.log('currentVersion', currentVersion);
-    console.log('latestVersion', latestVersion);
-    for (let i = 0; i < 3; i++) {
-        if (currentVersion[i] < latestVersion[i]) {
-            flag = true;
+    try {
+        if (latest.includes('beta')) {
+            // const isCheckBetaUpdate =
+            //     db.get('settings.checkBetaUpdate') !== false;
+            // if (!isCheckBetaUpdate) {
+            //     return false;
+            // }
         }
+        return lt(current, latest);
+    } catch (e) {
+        return false;
     }
-
-    return flag;
 };
 
 export default checkVersion;
